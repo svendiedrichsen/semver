8c116092a3caf945f0a7e2af9eae1b28849c2362
GeBeater <info@gebeat.com>
2017-01-31 14:18:23 +0100

    add mockito dependency and improve code coverage

---
+++ src/test/java/de/semver/AlphaNumericalVersionPartTest.java
@@ -0,0 +1,42 @@
+package de.semver;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class AlphaNumericalVersionPartTest {
+
+    @Test
+    public void testGetValue() throws Exception {
+        final String expectedVersionPart = "alpha";
+        AlphaNumericalVersionPart objectUnderTest = new AlphaNumericalVersionPart("alpha");
+        String actualVersionPart = objectUnderTest.getValue();
+        Assert.assertEquals(actualVersionPart, expectedVersionPart, "Value result is wrong.");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        final String expectedToStringValue = "RC1";
+        AlphaNumericalVersionPart objectUnderTest = new AlphaNumericalVersionPart("RC1");
+        String actualToStringValue = objectUnderTest.toString();
+        Assert.assertEquals(expectedToStringValue, actualToStringValue, "ToString result is wrong.");
+    }
+
+    @Test(dataProvider = "comparisonData")
+    public void testCompareTo(VersionPart o, VersionPart otherValue, int expectedComparisonResult) throws Exception {
+        AlphaNumericalVersionPart objectUnderTest = new AlphaNumericalVersionPart(o.toString());
+        int actualComparisonResult = objectUnderTest.compareTo(otherValue);
+        Assert.assertEquals(actualComparisonResult, expectedComparisonResult, "Comparison result is wrong.");
+    }
+
+    @DataProvider(name = "comparisonData")
+    public Object[][] createComparisonData() {
+        return new Object[][]{
+                { new AlphaNumericalVersionPart("alpha"), new NumericalVersionPart(23), 1},
+                { new AlphaNumericalVersionPart("alpha"), new AlphaNumericalVersionPart("beta"), -1},
+                { new AlphaNumericalVersionPart("alpha"), new AlphaNumericalVersionPart("alpha"), 0},
+                { new AlphaNumericalVersionPart("beta"), new AlphaNumericalVersionPart("alpha"), 1},
+        };
+    }
+
+}
---
+++ src/test/java/de/semver/BuildMetadataTest.java
@@ -0,0 +1,31 @@
+package de.semver;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
+public class BuildMetadataTest {
+
+    @Test
+    public void testSize() throws Exception {
+        final int expectedSize = 2;
+        BuildMetadata objectUnderTest = new BuildMetadata(mock(VersionPart.class), mock(VersionPart.class));
+        int actualSize = objectUnderTest.size();
+        Assert.assertEquals(actualSize, expectedSize, "Size result is wrong.");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        final String expectedToStringValue = "identifiers1.identifiers2";
+        VersionPart versionPart1Mock = mock(VersionPart.class);
+        doReturn("identifiers1").when(versionPart1Mock).getValue();
+        VersionPart versionPart2Mock = mock(VersionPart.class);
+        doReturn("identifiers2").when(versionPart2Mock).getValue();
+        BuildMetadata objectUnderTest = new BuildMetadata(versionPart1Mock, versionPart2Mock);
+        String actualToStringValue = objectUnderTest.toString();
+        Assert.assertEquals(actualToStringValue, expectedToStringValue, "ToString result is wrong.");
+        verify(versionPart1Mock, times(1)).getValue();
+        verify(versionPart2Mock, times(1)).getValue();
+    }
+}
